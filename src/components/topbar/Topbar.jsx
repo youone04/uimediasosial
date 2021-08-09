c1ab484000a0ef999a3eee3ac57bf305ce60568d
azzahra-jateng <azzahra.jateng@gmail.com>
2021-08-09 11:20:34 +0700

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,42 @@
+import './topbar.css';
+import { Search , Person ,Chat , Notifications } from "@material-ui/icons";
+
+const Topbar = () => {
+    return(
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">
+                    CawaApps
+                </span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchBar">
+                    <Search className="searchIcon"/>
+                    <input placeholder="Search for friends, post or video" type="text" className="searchInput" />
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarLink">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbariconItem">
+                        <Person/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbariconItem">
+                        <Chat/>
+                        <span className="topbarIconBadge">2</span>
+                    </div>
+                    <div className="topbariconItem">
+                        <Notifications/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+                <img src="/assets/person/1.jpeg" alt="" className="topbarImage" />
+            </div>
+        </div>
+    )
+}
+export default Topbar;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import Topbar from "../../components/topbar/Topbar"
+
+const Home = () => {
+    return(
+        <div>
+           <Topbar/>
+        </div>
+    )
+}
+export default Home;
