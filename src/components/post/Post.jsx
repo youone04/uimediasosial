bcd6f47535e27ea2309ba0ff7bb2902b0376afa9
youone04 <yudi.gunaone87@gmail.com>
2021-08-17 15:53:27 +0700

    update post

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,18 @@
+import './post.css';
+
+const Post = () => {
+    return(
+        <div className="post">
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postLeft"></div>
+                    <div className="postLeft"></div>
+                    <div className="postLeft"></div>
+                </div>
+                <div className="postCenter"></div>
+                <div className="postBottom"></div>
+            </div>
+        </div>
+    )
+}
+export default Post;
