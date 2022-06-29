88118bcf5f700ab574934ba5206d9aeb316b0588
youone04 <yudi.gunaone87@gmail.com>
2022-06-29 21:24:40 +0700

    update router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
-import Login from './pages/login/Login';
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-    return(
-        <>
-            {/* <Profile/> */}
-            {/* <Login/> */}
-            <Home/>
-        </>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App; 
+export default App;
