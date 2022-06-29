ca4277e45f723c17be0d8436cf755461246635a0
youone04 <yudi.gunaone87@gmail.com>
2022-06-29 21:42:29 +0700

    update componenst top bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,42 +1,47 @@
-import './topbar.css';
-import { Search , Person ,Chat , Notifications } from "@material-ui/icons";
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 const Topbar = () => {
-    return(
-        <div className="topbarContainer">
-            <div className="topbarLeft">
-                <span className="logo">
-                    CawaApps
-                </span>
-            </div>
-            <div className="topbarCenter">
-                <div className="searchBar">
-                    <Search className="searchIcon"/>
-                    <input placeholder="Search for friends, post or video" type="text" className="searchInput" />
-                </div>
-            </div>
-            <div className="topbarRight">
-                <div className="topbarLink">
-                    <span className="topbarLink">Homepage</span>
-                    <span className="topbarLink">Timeline</span>
-                </div>
-                <div className="topbarIcons">
-                    <div className="topbariconItem">
-                        <Person/>
-                        <span className="topbarIconBadge">1</span>
-                    </div>
-                    <div className="topbariconItem">
-                        <Chat/>
-                        <span className="topbarIconBadge">2</span>
-                    </div>
-                    <div className="topbariconItem">
-                        <Notifications/>
-                        <span className="topbarIconBadge">1</span>
-                    </div>
-                </div>
-                <img src="/assets/person/1.jpeg" alt="" className="topbarImage" />
-            </div>
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <Link to={"/"} style={{textDecoration:'none'}}>
+          <span className="logo">CawaApps</span>
+        </Link>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friends, post or video"
+            type="text"
+            className="searchInput"
+          />
         </div>
-    )
-}
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLink">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbariconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbariconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbariconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImage" />
+      </div>
+    </div>
+  );
+};
 export default Topbar;
